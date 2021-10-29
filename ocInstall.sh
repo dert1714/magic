763a31b8d339e26ad6c3db8939e571bc291275ac
Valerii Chuvashkin <vach0717@netcracker.com>
2021-10-29 19:19:20 +0400

    very important

--- ocInstall.sh
+++ ocInstall.sh
@@ -3,3 +3,5 @@
 cd /home/ubuntu/Downloads
 tar -xf openshift-origin-client-tools-v3.7.0-alpha.1-fdbd3dc-linux-64bit.tar.gz
 export PATH="$PATH:/home/ubuntu/Downloads/openshift-origin-client-tools-v3.7.0-alpha.1-fdbd3dc-linux-64bit"
+
+echo "aaaa"
